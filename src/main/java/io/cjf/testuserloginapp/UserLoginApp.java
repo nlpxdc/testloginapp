1bd80ee21883f6e6293663b5d7cd8b5aeb632e43
nlpxdc <nlpx_dc@hotmail.com>
2020-09-14 12:24:56 +0800

    first init

---
+++ src/main/java/io/cjf/testuserloginapp/UserLoginApp.java
@@ -0,0 +1,11 @@
+package io.cjf.testuserloginapp;
+
+import io.cjf.commonexception.ExceptionConstant;
+
+public class UserLoginApp {
+    public static void main(String[] args) {
+        String myError = ExceptionConstant.MY_ERROR;
+        String youError = ExceptionConstant.YOU_ERROR;
+        String taError = ExceptionConstant.TA_ERROR;
+    }
+}
